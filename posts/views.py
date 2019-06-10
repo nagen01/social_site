260fe246b290d3f51dfb7ec331beb476fde0af6a
nagen01 <50768930+nagen01@users.noreply.github.com>
2019-06-10 21:49:55 +0530

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,8 @@ from django.urls import reverse_lazy
 from django.http import Http404
 from django.views import generic
 from braces.views import SelectRelatedMixin
-from . import models
+from posts import models
 from django.contrib import messages
-from . import forms
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
